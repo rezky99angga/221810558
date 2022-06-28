6ca6d7163fb307084914327173ec2e3ae8d5a754
rezky99angga <70829900+rezky99angga@users.noreply.github.com>
2022-06-29 00:10:26 +0700

    Update database

--- masukid3_221810558.sql
+++ masukid3_221810558.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 4.9.7
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 20, 2022 at 08:29 AM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.3
+-- Host: localhost:3306
+-- Generation Time: Jun 28, 2022 at 10:49 PM
+-- Server version: 10.3.34-MariaDB-cll-lve
+-- PHP Version: 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `surat`
+-- Database: `masukid3_221810558`
 --
 
 -- --------------------------------------------------------
@@ -44,7 +44,8 @@ CREATE TABLE `disposisi` (
 INSERT INTO `disposisi` (`id_disposisi`, `id_surat`, `id_pegawai_pengirim`, `id_pegawai_penerima`, `tgl_disposisi`, `keterangan`) VALUES
 (21, 10, 15, 4, '2022-06-20 02:14:33', 'Dari BPS Banyumas'),
 (22, 10, 15, 18, '2022-06-20 02:15:54', 'Surat tolong dicek'),
-(25, 14, 15, 4, '2022-06-20 06:19:10', 'koordinator cek');
+(25, 14, 15, 4, '2022-06-20 06:19:10', 'koordinator cek'),
+(26, 10, 15, 18, '2022-06-20 10:14:48', 'Coba Disposisi');
 
 -- --------------------------------------------------------
 
@@ -54,7 +55,7 @@ INSERT INTO `disposisi` (`id_disposisi`, `id_surat`, `id_pegawai_pengirim`, `id_
 
 CREATE TABLE `jabatan` (
   `id_jabatan` int(11) NOT NULL,
-  `nama_jabatan` varchar(100) NOT NULL,
+  `nama_jabatan` char(30) NOT NULL,
   `role` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -63,10 +64,11 @@ CREATE TABLE `jabatan` (
 --
 
 INSERT INTO `jabatan` (`id_jabatan`, `nama_jabatan`, `role`) VALUES
-(1, 'Bagian Umum', 1),
+(1, 'Sub Bagian Umum', 1),
 (2, 'Kepala BPS', 2),
-(3, 'Koordinator', 3),
-(4, 'Keamanan', 4);
+(3, 'Koordinator Fungsi 1', 3),
+(4, 'Keamanan', 4),
+(11, 'Koordinator Fungsi 2', 3);
 
 -- --------------------------------------------------------
 
@@ -77,7 +79,7 @@ INSERT INTO `jabatan` (`id_jabatan`, `nama_jabatan`, `role`) VALUES
 CREATE TABLE `pegawai` (
   `id_pegawai` int(11) NOT NULL,
   `username` char(20) NOT NULL,
-  `nama_pegawai` varchar(200) NOT NULL,
+  `nama_pegawai` char(50) NOT NULL,
   `id_jabatan` int(11) NOT NULL,
   `password` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -87,11 +89,11 @@ CREATE TABLE `pegawai` (
 --
 
 INSERT INTO `pegawai` (`id_pegawai`, `username`, `nama_pegawai`, `id_jabatan`, `password`) VALUES
-(2, 'kepalabps', 'Kepala BPS', 2, '21232f297a57a5a743894a0e4a801fc3'),
-(3, 'keamanan4', 'Keamanan', 4, '21232f297a57a5a743894a0e4a801fc3'),
-(4, 'koordinator3', 'Koordinator', 3, '21232f297a57a5a743894a0e4a801fc3'),
-(15, 'bagianumum', 'Sub Bagian Umum', 1, '21232f297a57a5a743894a0e4a801fc3'),
-(18, 'koordinator2', 'Koordinator 2', 3, '21232f297a57a5a743894a0e4a801fc3');
+(1, 'bagianumum', 'Heni Yulianti, SST, M.Si', 1, '21232f297a57a5a743894a0e4a801fc3'),
+(2, 'kepalabps', 'Djulfikar Rizky, M.Si', 2, '21232f297a57a5a743894a0e4a801fc3'),
+(3, 'keamanan', 'Keamanan', 4, '21232f297a57a5a743894a0e4a801fc3'),
+(4, 'koordinator2', 'Koordinator Fungsi 2', 11, '21232f297a57a5a743894a0e4a801fc3'),
+(18, 'koordinator1', 'Koordinator Fungsi 1', 3, '21232f297a57a5a743894a0e4a801fc3');
 
 -- --------------------------------------------------------
 
@@ -126,6 +128,7 @@ CREATE TABLE `surat_keluar` (
   `tgl_kirim` date NOT NULL,
   `tujuan` varchar(200) NOT NULL,
   `perihal` varchar(200) NOT NULL,
+  `id_pegawai` int(11) NOT NULL DEFAULT 1,
   `file_surat` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -133,8 +136,9 @@ CREATE TABLE `surat_keluar` (
 -- Dumping data for table `surat_keluar`
 --
 
-INSERT INTO `surat_keluar` (`id_surat`, `nomor_surat`, `tgl_kirim`, `tujuan`, `perihal`, `file_surat`) VALUES
-(8, '432/suratkeluar1', '2022-06-20', 'BPS Bali', 'Undangan Rapat', 'Contoh_Surat3.pdf');
+INSERT INTO `surat_keluar` (`id_surat`, `nomor_surat`, `tgl_kirim`, `tujuan`, `perihal`, `id_pegawai`, `file_surat`) VALUES
+(8, '432/suratkeluar1', '2022-06-20', 'BPS Bali', 'Undangan Rapat', 1, 'Contoh_Surat3.pdf'),
+(9, '222/suratkeluar2', '2022-06-24', 'Jakarta', 'Undangan', 1, 'Contoh_Surat6.pdf');
 
 -- --------------------------------------------------------
 
@@ -143,10 +147,10 @@ INSERT INTO `surat_keluar` (`id_surat`, `nomor_surat`, `tgl_kirim`, `tujuan`, `p
 --
 
 CREATE TABLE `surat_keputusan` (
-  `id_surat` int(10) NOT NULL,
-  `nomor_surat` varchar(40) NOT NULL,
+  `id_surat` int(11) NOT NULL,
+  `nomor_surat` varchar(200) NOT NULL,
   `tgl_surat` date NOT NULL,
-  `perihal` varchar(100) NOT NULL,
+  `perihal` varchar(200) NOT NULL,
   `bagian` int(11) NOT NULL,
   `file_surat` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -171,6 +175,7 @@ CREATE TABLE `surat_masuk` (
   `tgl_terima` date NOT NULL,
   `pengirim` varchar(200) NOT NULL,
   `perihal` varchar(200) NOT NULL,
+  `id_pegawai` int(11) NOT NULL DEFAULT 1,
   `file_surat` text NOT NULL,
   `status` enum('proses','selesai') NOT NULL DEFAULT 'proses'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -179,10 +184,11 @@ CREATE TABLE `surat_masuk` (
 -- Dumping data for table `surat_masuk`
 --
 
-INSERT INTO `surat_masuk` (`id_surat`, `nomor_surat`, `tgl_kirim`, `tgl_terima`, `pengirim`, `perihal`, `file_surat`, `status`) VALUES
-(10, '111/surat1', '2022-06-18', '2022-06-20', 'BPS Banyumas', 'Undangan Rapat', 'Contoh_Surat.pdf', 'proses'),
-(13, '333/surat3', '2022-06-16', '2022-06-20', 'BPS Pusat', 'Keputusan Kepala BPS Pusat', 'Contoh_Surat1.pdf', 'proses'),
-(14, '222/surat2', '2022-06-15', '2022-06-20', 'BPS Banyumas', 'Undangan', 'Contoh_Surat4.pdf', 'proses');
+INSERT INTO `surat_masuk` (`id_surat`, `nomor_surat`, `tgl_kirim`, `tgl_terima`, `pengirim`, `perihal`, `id_pegawai`, `file_surat`, `status`) VALUES
+(10, '111/surat1', '2022-06-18', '2022-06-20', 'BPS Banyumas', 'Undangan Rapat', 1, 'Contoh_Surat.pdf', 'proses'),
+(13, '333/surat3', '2022-06-16', '2022-06-20', 'BPS Pusat', 'Keputusan Kepala BPS Pusat', 1, 'Contoh_Surat1.pdf', 'proses'),
+(14, '222/surat2', '2022-06-15', '2022-06-20', 'BPS Banyumas', 'Undangan', 1, 'Contoh_Surat4.pdf', 'proses'),
+(15, '444/surat4', '2022-06-17', '2022-06-20', 'BPS Banyumas', 'Rapat', 1, 'Contoh_Surat5.pdf', 'proses');
 
 -- --------------------------------------------------------
 
@@ -191,20 +197,21 @@ INSERT INTO `surat_masuk` (`id_surat`, `nomor_surat`, `tgl_kirim`, `tgl_terima`,
 --
 
 CREATE TABLE `tamu` (
-  `id_tamu` int(10) NOT NULL,
-  `nama_tamu` varchar(30) NOT NULL,
+  `id_tamu` int(11) NOT NULL,
+  `nama_tamu` char(50) NOT NULL,
   `tgl_masuk` date NOT NULL,
-  `alamat` varchar(100) NOT NULL,
-  `keperluan` varchar(100) NOT NULL,
-  `suhu` int(3) NOT NULL
+  `alamat` varchar(200) NOT NULL,
+  `keperluan` varchar(200) NOT NULL,
+  `suhu` int(2) NOT NULL,
+  `id_pegawai` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tamu`
 --
 
-INSERT INTO `tamu` (`id_tamu`, `nama_tamu`, `tgl_masuk`, `alamat`, `keperluan`, `suhu`) VALUES
-(7, 'Galih', '2022-06-20', 'Bobotsari', 'Mengambil berkas', 34);
+INSERT INTO `tamu` (`id_tamu`, `nama_tamu`, `tgl_masuk`, `alamat`, `keperluan`, `suhu`, `id_pegawai`) VALUES
+(7, 'Galih', '2022-06-20', 'Bobotsari', 'Mengambil berkas', 34, 4);
 
 --
 -- Indexes for dumped tables
@@ -246,7 +253,8 @@ ALTER TABLE `role`
 --
 ALTER TABLE `surat_keluar`
   ADD PRIMARY KEY (`id_surat`),
-  ADD KEY `id_surat` (`id_surat`);
+  ADD KEY `id_surat` (`id_surat`),
+  ADD KEY `id_pegawai_id_pegawai` (`id_pegawai`);
 
 --
 -- Indexes for table `surat_keputusan`
@@ -259,14 +267,16 @@ ALTER TABLE `surat_keputusan`
 -- Indexes for table `surat_masuk`
 --
 ALTER TABLE `surat_masuk`
-  ADD PRIMARY KEY (`id_surat`);
+  ADD PRIMARY KEY (`id_surat`),
+  ADD KEY `id_id` (`id_pegawai`);
 
 --
 -- Indexes for table `tamu`
 --
 ALTER TABLE `tamu`
   ADD PRIMARY KEY (`id_tamu`),
-  ADD UNIQUE KEY `id_tamu` (`id_tamu`);
+  ADD UNIQUE KEY `id_tamu` (`id_tamu`),
+  ADD KEY `tamu_role_role_id_role` (`id_pegawai`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -276,19 +286,19 @@ ALTER TABLE `tamu`
 -- AUTO_INCREMENT for table `disposisi`
 --
 ALTER TABLE `disposisi`
-  MODIFY `id_disposisi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id_disposisi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `jabatan`
 --
 ALTER TABLE `jabatan`
-  MODIFY `id_jabatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_jabatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `pegawai`
 --
 ALTER TABLE `pegawai`
-  MODIFY `id_pegawai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id_pegawai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `role`
@@ -300,25 +310,25 @@ ALTER TABLE `role`
 -- AUTO_INCREMENT for table `surat_keluar`
 --
 ALTER TABLE `surat_keluar`
-  MODIFY `id_surat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_surat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `surat_keputusan`
 --
 ALTER TABLE `surat_keputusan`
-  MODIFY `id_surat` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_surat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `surat_masuk`
 --
 ALTER TABLE `surat_masuk`
-  MODIFY `id_surat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_surat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `tamu`
 --
 ALTER TABLE `tamu`
-  MODIFY `id_tamu` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_tamu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
@@ -344,10 +354,28 @@ ALTER TABLE `pegawai`
   ADD CONSTRAINT `pegawai_jabatan_id_jabatan_fk` FOREIGN KEY (`id_jabatan`) REFERENCES `jabatan` (`id_jabatan`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
+-- Constraints for table `surat_keluar`
+--
+ALTER TABLE `surat_keluar`
+  ADD CONSTRAINT `id_pegawai_id_pegawai` FOREIGN KEY (`id_pegawai`) REFERENCES `pegawai` (`id_pegawai`);
+
+--
 -- Constraints for table `surat_keputusan`
 --
 ALTER TABLE `surat_keputusan`
   ADD CONSTRAINT `bagian_id_jabatan` FOREIGN KEY (`bagian`) REFERENCES `jabatan` (`id_jabatan`);
+
+--
+-- Constraints for table `surat_masuk`
+--
+ALTER TABLE `surat_masuk`
+  ADD CONSTRAINT `id_id` FOREIGN KEY (`id_pegawai`) REFERENCES `pegawai` (`id_pegawai`);
+
+--
+-- Constraints for table `tamu`
+--
+ALTER TABLE `tamu`
+  ADD CONSTRAINT `tamu_role_role_id_role` FOREIGN KEY (`id_pegawai`) REFERENCES `pegawai` (`id_pegawai`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
